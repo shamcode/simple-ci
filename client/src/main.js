06d8e3b6afb38ed336a8fe21d6b7fc23e5a382a6
Eugeny Burnashov <shamcode@gmail.com>
2018-04-06 19:22:11 +0700

    Update dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -7,6 +7,13 @@ if ( module.hot ) {
     const app = DI.resolve( 'widgets:app' );
     if ( app !== undefined ) {
         app.remove();
+        DI.resolve( 'sham-ui' ).render.widgets.forEach( widget => {
+            try {
+                widget.remove();
+            } catch ( e ) {
+
+            }
+        } )
     }
     module.hot.accept();
 }
