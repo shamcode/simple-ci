66846eeb0d6ebf4554d46b696bd16d877bf2656a
Eugeny Burnashov <shamcode@gmail.com>
2018-04-09 11:42:04 +0700

    Update snapshots

--- client/__tests__/unit/widgets/Header.js
+++ client/__tests__/unit/widgets/Header.js
@@ -1,7 +1,16 @@
+import { DI } from 'sham-ui';
 import Header from '../../../src/widgets/Header.sht';
 import renderer from 'sham-ui-test-helpers';
 
+afterEach( () => {
+    DI.bind( 'router', null );
+} );
+
 it( 'renders correctly', () => {
+    DI.bind( 'router', {
+        generate: jest.fn().mockReturnValueOnce( '/' )
+    } );
+
     const meta = renderer( Header, {} );
     expect( meta.toJSON() ).toMatchSnapshot();
 } );
