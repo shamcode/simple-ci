1cc2836b1c54f3a0b0b30485d3953ae4add290f4
Eugeny Burnashov <shamcode@gmail.com>
2018-05-07 20:27:23 +0700

    Small refactoring

--- client/src/services/store.js
+++ client/src/services/store.js
@@ -7,22 +7,29 @@ export default class Store {
         this.axios = axios.create( {
             baseURL: 'http://localhost:3001/api/v1/'
         } );
-        this.axios.interceptors.response.use( ( response ) => {
-            if ( !this.session.checkToken( response.headers[ 'Bearer' ] ) ) {
-                this.session.invalidateSession();
-                return Promise.reject();
-            }
-            return response;
-        }, ( error ) => {
-            if ( error.response && 403 === error.response.status ) {
-                this.session.invalidateSession();
-            }
-            return Promise.reject( error );
-        } )
+        this.axios.interceptors.response.use(
+            ::this._responseSuccessAuthInterceptor,
+            ::this._responseFailAuthInterceptor
+        )
     }
 
     get session() {
         return DI.resolve( 'session' );
+    }
+
+    _responseSuccessAuthInterceptor( response ) {
+        if ( !this.session.checkToken( response.headers[ 'Bearer' ] ) ) {
+            this.session.invalidateSession();
+            return Promise.reject();
+        }
+        return response;
+    }
+
+    _responseFailAuthInterceptor( error ) {
+        if ( error.response && 403 === error.response.status ) {
+            this.session.invalidateSession();
+        }
+        return Promise.reject( error );
     }
 
     setAuthHeaders( token ) {
