e8a882818aa7179a5252280d905671441eea4361
Eugeny Burnashov <shamcode@gmail.com>
2018-07-06 19:58:40 +0700

    Socket service

--- client/src/controllers/main.js
+++ client/src/controllers/main.js
@@ -1,6 +1,7 @@
 import { DI } from 'sham-ui';
 import Session from '../services/session';
 import Store from '../services/store';
+import Socket from '../services/socket';
 import startRouter from './routes';
 import disabled from '../directives/disabled';
 import EventListenerDirectives from '../directives/event-listener';
@@ -9,6 +10,7 @@ import App from '../widgets/App.sht';
 export default function() {
     new Store();
     new Session();
+    new Socket();
 
     startRouter();
 
